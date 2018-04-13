247373d349b99650ff388b674db3e752c4ac8e0c
sunhao <332449142@qq.com>
2018-04-13 17:10:11 +0800

    config的自动刷新卡在没有安装rabbitmq服务器

---
+++ config_client/src/main/java/com/sunhao/config_client/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.sunhao.config_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${my.name}")
+    private String name;
+
+    @Value("${my.age}")
+    private int age;
+
+    @GetMapping("/getConfig")
+    public String getConfig() {
+        return "I'm " + name + ", and I'm " + age;
+    }
+}
---
+++ eureka_zuul_client/src/main/java/com/sunhao/eureka_zuul_client/my_filter/TokenFilter.java
@@ -0,0 +1,62 @@
+package com.sunhao.eureka_zuul_client.my_filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String name = request.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            return true;
+        } else if ("sunhao".equals(name)){
+            return true;
+        } else {
+            //在这里返回false之后不执行run方法，如果zuul有熔断器的话直接执行快速失败
+            return false;
+        }
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        try {
+            request.setCharacterEncoding("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+        String token = request.getParameter("token");
+        if (token == null || token.isEmpty()) {
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(10086);
+
+            try {
+                context.getResponse().getWriter().write("token 不能为空！");
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
